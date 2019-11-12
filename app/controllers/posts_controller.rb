e01eeaa2d0b2a3266888da3972551bf6173136dd
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-12 14:50:23 -0800

    rubocop autofix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create]
+  before_action :logged_in_user, only: %i[new create]
 
   def index
     @posts = Post.paginate(page: params[:page])
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
 
     if @post.save
-      flash[:success] = "Post successfully created"
+      flash[:success] = 'Post successfully created'
       redirect_to posts_path
     else
       render 'new'
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
   def logged_in_user
     unless logged_in?
       store_location
-      flash[:danger] = "Please log in"
+      flash[:danger] = 'Please log in'
       redirect_to login_path
     end
   end
