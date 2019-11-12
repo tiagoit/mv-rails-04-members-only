f409e7106a35b7adf84690efa6799dc2df0378ac
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-12 14:28:55 -0800

    posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create]
 
-  def index; end
+  def index
+    @posts = Post.paginate(page: params[:page])
+  end
 
   def new
     @post = Post.new
